2573b4b3edc129b38701b1277bf9d4c48259f49f
Gabriel Hołubowicz <holubowicz.g@gmail.com>
2022-05-29 12:21:46 +0200

    stworzenie tabeli

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,197 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+      <>
+    <div className='firma'>
+        Nazwa firmy........
+    </div>
+    <div className='bilans'>
+        BILANS
+    </div>
+    <div className='rozliczenie'>
+        Dzień rozliczenia...
+    </div>
+    <div className='jednostka'>
+        Jednostka obliczeniowa: zł  
+    </div>
+    <div className='tabela'>
+        <table className='table'>
+            <tr >
+                <th> </th>
+                <th className='glowne'>AKTYWA</th>
+                <th className='glowne'>Dzień kończący</th>
+                <th > </th>
+                <th className='glowne'>PASYWA</th>
+                <th className='glowne'>Dzień kończący</th>
+            </tr>
+            <tr >
+                <td className='kategoria'>A</td>
+                <td className='kategoria'>Aktywa trwałe</td>
+                <td className='kategoria'>XXX zł</td>
+                <td className='kategoria'>A </td>
+                <td className='kategoria'>Kapitał własny</td>
+                <td className='kategoria'>XXX zł</td>
+            </tr><tr>
+                <td className='podkategoria'>I</td>
+                <td className='podkategoria'>Wartości niematerialne i prawne</td>
+                <td className='podkategoria'>XXX zł</td>
+                <td className='podkategoria'>I </td>
+                <td className='podkategoria'>Kapitał Postawowy</td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+                <td className='podkategoria'>II</td>
+                <td className='podkategoria'>Rzeczowe aktywa trwałe, w tym:</td>
+                <td className='podkategoria'>XXX zł</td>
+                <td className='podkategoria'>II </td>
+                <td className='podkategoria'>Kapitał zapasowy w tym:</td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+                <td> </td>
+                <td>środki trwałe</td>
+                <td>XXX zł</td>
+                <td>  </td>
+                <td>Nadwyżka wartości sprzedaży nad wartością nominalną udziałów</td>
+                <td>XXX zł</td>
+            </tr><tr>
+                <td> </td>
+                <td>Środki trwałe w budowie</td>
+                <td>XXX zł</td>
+                <td className='podkategoria'>III </td>
+                <td className='podkategoria'>Kapitał z aktualizacji wyceny, w tym:</td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+                <td className='podkategoria'>III</td>
+                <td className='podkategoria'>Należności długoterminowe</td>
+                <td className='podkategoria'>XXX zł</td>
+                <td>  </td>
+                <td>z tytułu aktualizacji wartości godzinowej</td>
+                <td>XXX zł</td>
+            </tr><tr>
+                <td className='podkategoria'>IV</td>
+                <td className='podkategoria'>Inwestycje długoterminowe, w tym: </td>
+                <td className='podkategoria'> XXX zł</td>
+                <td className='podkategoria'>IV</td>
+                <td className='podkategoria'>Pozostałe kapitały rezerwowe</td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+                <td> </td>
+                <td>nieruchomości</td>
+                <td>XXX zł</td>
+                <td className='podkategoria'>V</td>
+                <td className='podkategoria'>Zysk/strata z lat ubiegłych </td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+                <td> </td>
+                <td>długoterminowe aktywa finansowe</td>
+                <td>XXX zł</td>
+                <td className='podkategoria'>VI </td>
+                <td className='podkategoria'>Zysk/ strata netto</td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+                <td className='podkategoria'>V</td>
+                <td className='podkategoria'>Długoterminowe rozliczenia międzyokresowe</td>
+                <td className='podkategoria'>XXX zł</td>
+                <td className='podkategoria'>VII  </td>
+                <td className='podkategoria'>Odpisy z zysku netto w ciągu roku obrotowego</td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+                <td className='kategoria'>B</td>
+                <td className='kategoria'>Aktywa obrotowe</td>
+                <td className='kategoria'>XXX zł</td>
+                <td className='kategoria'>B </td>
+                <td className='kategoria'>Zobowiązania i rezerwy na zobowiązania</td>
+                <td className='kategoria'>XXX zł</td>
+            </tr><tr>
+                <td className='podkategoria'>I</td>
+                <td className='podkategoria'>Zapasy</td>
+                <td className='podkategoria'>XXX zł</td>
+                <td className='podkategoria'>I</td>
+                <td className='podkategoria'>Rezerwy na zobowiązania, w tym: </td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+                <td className='podkategoria'>II</td>
+                <td className='podkategoria'>Należności, krótkotermiowe, w tym: </td>
+                <td className='podkategoria'>XXX zł</td>
+                <td >  </td>
+                <td>Rezerwa na świadczenia emerytalne i podobne</td>
+                <td>XXX zł</td>
+            </tr><tr>
+                <td> a </td>
+                <td>z tytułu dostaw i usług, w tym:</td>
+                <td>XXX zł</td>
+                <td className='podkategoria'>II </td>
+                <td className='podkategoria'>Zobowiązania długoterminowe, w tym</td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+                <td> </td>
+                <td>do 12 miesięcy</td>
+                <td>XXX zł</td>
+                <td> </td>
+                <td>z tytułu kredytów i pożyczek</td>
+                <td>XXX zł</td>
+            </tr><tr>
+                <td> </td>
+                <td>powyżej 12 miesięcy</td>
+                <td>XXX zł</td>
+                <td className='podkategoria'>III </td>
+                <td className='podkategoria'>Zobowiązania krótkotermiowe, w tym: </td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+                <td className='podkategoria'>III </td>
+                <td className='podkategoria'> Inwestycje krótkotermiowe w tym: </td>
+                <td className='podkategoria'>XXX zł</td>
+                <td>a </td>
+                <td>Z tytułu kredytów i pożyczek</td>
+                <td>XXX zł</td>
+            </tr><tr>
+                <td>a</td>
+                <td>krótkotermiowe aktywa finansowe, w tym: </td>
+                <td>XXX zł</td>
+                <td>b </td>
+                <td>z tytułu dostaw i usług, w tym: </td>
+                <td>XXX zł</td>
+            </tr><tr>
+                <td> </td>
+                <td>środki pieniężne w kasie i na rachunkach</td>
+                <td>XXX zł</td>
+                <td>  </td>
+                <td>do 12 miesięcy</td>
+                <td>XXX zł</td>
+            </tr><tr>
+                <td className='podkategoria'>IV</td>
+                <td className='podkategoria'>krótkotermiowe rozliczenia międzyokresowe</td>
+                <td className='podkategoria'>XXX zł</td>
+                <td> </td>
+                <td>powyżej 12 miesięcy</td>
+                <td>XXX zł</td>
+            </tr><tr>
+                <td className='kategoria'>C</td>
+                <td className='kategoria'>Należne wpłaty na kapitał podstawowy</td>
+                <td className='kategoria'>XXX zł</td>
+                <td>c </td>
+                <td>Fundusze specjalne</td>
+                <td>XXX zł</td>
+            </tr><tr>
+                <td className='kategoria'>D</td>
+                <td className='kategoria'>Udziały własne</td>
+                <td className='kategoria'>XXX zł</td>
+                <td className='podkategoria'>IV </td>
+                <td className='podkategoria'>Rozliczenia międzyokresowe</td>
+                <td className='podkategoria'>XXX zł</td>
+            </tr><tr>
+            <th> </th>
+                <th className='glowne'>AKTYWA razem</th>
+                <th className='glowne'>xxx zł</th>
+                <th className='glowne'> </th>
+                <th className='glowne'>PASYWA razem</th>
+                <th className='glowne'>xxx zł</th>
+            </tr>
+        </table>
+    </div>
+    </>
+  )
+}
+
+export default Home
