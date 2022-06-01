36c8764e45f4c1fa422560dc2e27fd56955cfe18
NotSoSaneExile <sciemastyle@wp.pl>
2022-06-02 01:48:38 +0200

    Zmiany w wyglądzie

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
+//import Header from "./components/Header";
 import Home from "./components/Home";
 function App() {
   return (
    <>
-   <Header/>
+   {/* <Header/> */}
    <Home/>
    </>
   );
