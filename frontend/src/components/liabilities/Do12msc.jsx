e818219dbed923f5b495b8489da5c9f778924d7a
NotSoSaneExile <sciemastyle@wp.pl>
2022-06-02 12:26:29 +0200

    cd

---
+++ frontend/src/components/liabilities/Do12msc.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function do12msc() {
+  return (
+    <div>do12msc</div>
+  )
+}
---
+++ frontend/src/components/liabilities/FunduszeSpecjalne.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FunduszeSpecjalne() {
+  return (
+    <div>FunduszeSpecjalne</div>
+  )
+}
