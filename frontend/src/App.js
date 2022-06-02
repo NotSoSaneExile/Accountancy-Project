f27492d650023afcfb5d5f451aeea9133d29f85a
NotSoSaneExile <sciemastyle@wp.pl>
2022-06-02 21:39:56 +0200

    xyz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 //import Header from "./components/Header";
 import Home from "./components/Home";
+import { jsPDF } from "jspdf";
+
+// Default export is a4 paper, portrait, using millimeters for units
+const doc = new jsPDF();
+
+doc.text("Hello world!", 10, 10);
+doc.save("a4.pdf");
+
 function App() {
   return (
    <>
